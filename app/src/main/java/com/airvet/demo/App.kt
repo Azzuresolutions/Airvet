7737c58de7421607688de808db7c9dea7e7bf6c7
Azzuresolutions <dharmikj.patel66@gmail.com>
2022-08-09 19:23:28 +0530

    init

--- app/src/main/java/com/airvet/demo/App.kt
+++ app/src/main/java/com/airvet/demo/App.kt
@@ -7,7 +7,7 @@ import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
 
 class App : Application() {
-
+a
     override fun onCreate() {
         super.onCreate()
         initKoin()
